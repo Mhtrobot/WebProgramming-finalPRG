9e3e308c2c0379574f54ee47a0bc5ad8c0bb9ca0
Mhtrobot <mhtrobot760@gmail.com>
2024-06-24 20:15:31 +0330

    login token finish

--- back-end-Project/main.py
+++ back-end-Project/main.py
@@ -1,6 +1,8 @@
+from datetime import timedelta
 from typing import Annotated
 
 from fastapi import FastAPI, Depends, HTTPException,status, Header
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 
 import auth
@@ -27,3 +29,21 @@ def create_user(user: schemas.UserBase, db: Annotated[Session, Depends(get_db)])
     db.refresh(db_user)
     return db_user
 
+@app.post('/login-token')
+async def login_for_access_token(db: Annotated[Session, Depends(get_db)], form_data: OAuth2PasswordRequestForm = Depends()):
+    user = db.query(models.USERS).filter(models.USERS.email == form_data.username).first()
+    if not user or not auth.verify_password(form_data.password, user.password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect email or password",
+            headers={"WWW-Authenticate": "Bearer"}
+        )
+    access_token_expires = timedelta(minutes=auth.ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = auth.create_access_token(
+        data={"sub": user.email}, expires_delta=access_token_expires
+    )
+    return {
+        "message": "Login Successful",
+        "user_detail": user,
+        "access_token": access_token, "token_type": "bearer"
+    }
