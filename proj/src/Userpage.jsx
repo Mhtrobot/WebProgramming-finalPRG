dcc082b251b551cd0bbe98ce123ae94617d4204f
Mhtrobot <mhtrobot760@gmail.com>
2024-07-08 19:20:43 +0330

    userpage features added

--- proj/src/Userpage.jsx
+++ proj/src/Userpage.jsx
@@ -38,7 +38,6 @@ function Userpage() {
             const response = await fetch(`http://127.0.0.1:8000/list-of-todos/${userId}`, {method: 'GET'});
             const data = await response.json();
             setList(data)
-            setList(list.reverse())
         } catch (error) {
             console.error('Error Fetching data: ', error)
         }
@@ -174,7 +173,41 @@ function Userpage() {
                             <p className='ml-5'>Todo On Progress : {list.filter(todo => todo.status === 'false').length} </p>
                         </div>
 
-
+                        <div className={'list w-50'}>
+                            <ul className={"list-group"}>
+                                {list.map((todo, index)=>(
+                                    <div key={index}
+                                         className={`todo container-fluid mt-2 p-3 ${todo.status === 'true' ? 'grayed-out' : ''}`}>
+                                        <li className={'col-lg-7 mt-1'}>
+                                            {todo.todo}
+                                        </li>
+                                        <div className={`icon col-lg-4 ${todo.status === 'true' ? 'grayed-out' : ''}`}>
+                                            <button onClick={() => deleteTodo(todo.todo_id)} className={`mt-1 mr-2`}>
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25"
+                                                     fill="red" className={`bi bi-trash ${todo.status === 'true' ? 'grayed-out' : ''}`} viewBox="0 0 16 16">
+                                                    <path
+                                                        d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
+                                                    <path
+                                                        d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
+                                                </svg>
+                                            </button>
+                                            <button onClick={() => markAsDone(todo.todo_id)}
+                                                    disabled={todo.status === 'true'} className='mt-1'>
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
+                                                     fill="green" className={`bi bi-clipboard-check ${todo.status === 'true' ? 'grayed-out' : ''}`} viewBox="0 0 16 16">
+                                                    <path fillRule="evenodd"
+                                                          d="M10.854 7.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 9.793l2.646-2.647a.5.5 0 0 1 .708 0"/>
+                                                    <path
+                                                        d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1z"/>
+                                                    <path
+                                                        d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0z"/>
+                                                </svg>
+                                            </button>
+                                        </div>
+                                    </div>
+                                ))}
+                            </ul>
+                        </div>
 
                         <button type={"button"} onClick={logOut} className='btn btn-danger logout'>LogOut</button>
                     </div>
